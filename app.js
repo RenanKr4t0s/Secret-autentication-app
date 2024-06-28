e6dea6abea95c4cbac8c53a996cf5d5ca51d83fb
RenanKr4t0s <redrums88@gmail.com>
2024-06-28 18:16:15 -0300

    Cadastro com criptografia finalizado

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const mongoose = require (`mongoose`)
+const encrypt = require('mongoose-encryption');
 const ejs = require('ejs')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/usersDb')
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+let UserBank = mongoose.model('users',userSchema)
+
 
 
 app.get('/', (req, res) => {
@@ -20,6 +37,30 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
+app.post('/register', async(req,res)=>{
+    try {
+        const newUser = new UserBank({
+            email: req.body.email,
+            password: req.body.password
+        })
+        await newUser.save().then(()=>console.log('user saved'+newUser))
+        res.render('secrets')
+        
+    } catch (error) {
+        console.log("Oops, algo deu errado ",error)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const testEmail =  req.body.email
+        const testPassword =  req.body.password
+        isRegistered = await UserBank.findOne({email:testEmail, password: testPassword}).exec()
+        isRegistered ? res.render('secrets') : res.send('<h2> Dados incorretos para logon</h2>')
+    } catch (error) {
+        console.log("Oops, algo deu errado ",error)
+    }
+})
 
 app.listen(4000, () => {
     console.log('Servidor Rodando na porta 4000')
